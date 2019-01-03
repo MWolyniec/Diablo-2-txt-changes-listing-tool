bc2f2f1f1d4dec7e598a45b3ee0db786c2315ed9
Mateusz Wołyniec <mwolyniec@10g.pl>
2019-01-03 09:00:35 +0100

    Looks for "old.txt" and "new.txt" in the current folder, but you may comment it out and uncomment the lines letting you enter path of both files during runtime.

---
+++ TxtComparerConsole/Comparer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TxtComparerConsole
+{
+    class Comparer
+    {
+        private FileInfo[] files;
+        string[] original;
+        string[] changed;
+        string[] fieldNames;
+        string[] linieNazwy;
+
+        internal List<string[]> Compare(FileInfo[] files)
+        {
+
+            original = File.ReadAllLines(files[0].FullName);
+            changed = File.ReadAllLines(files[1].FullName);
+           
+
+            List<string[]> changes = new List<string[]>();
+
+            fieldNames = original[0].Split('\t');
+
+            linieNazwy = new string[changed.Length];
+
+            for(int i = 0; i < changed.Length; i++)
+            {
+                if(original[i] != null)
+                {
+                    changes.Add(CompareLine(i, original[i], changed[i]));
+                }
+                else changes.Add(new string[] { $"Added line nr {i}: "+ changed[i]} );
+            }
+            return changes;
+        }
+
+        private string[] CompareLine(int nrLinii, string v1, string v2)
+        {
+            string[] orglinia = v1.Split('\t');
+            string[] zmienlinia = v2.Split('\t');
+
+            string[] zmiany = new string[zmienlinia.Length];
+
+            linieNazwy[nrLinii] = zmienlinia[0];
+
+            for(int fieldNr = 0; fieldNr < zmienlinia.Length; fieldNr++)
+            {
+
+                if(orglinia[fieldNr] != zmienlinia[fieldNr])
+                    zmiany[fieldNr] = $"{linieNazwy[nrLinii]}, (line {nrLinii}), field {fieldNames[fieldNr]}({fieldNr}): \"" + orglinia[fieldNr] + "\" => \"" + zmienlinia[fieldNr] + "\"";
+            }
+            return zmiany;
+        }
+    }
+}
--- TxtComparerConsole/Program.cs
+++ TxtComparerConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace TxtComparerConsole
     {
         static void Main(string[] args)
         {
+            FileInfo[] files = new FileInfo[2];
+            /*
+            for (int i = 0; i < files.Length;)
+             {
+                 Console.WriteLine($"Input file {i+1}: ");
+                 files[i] = new FileInfo(Console.ReadLine());
+                 if(files[i].Exists) i++;
+                 else Console.WriteLine("File doesn't exist, try again.");
+             }*/
+            files[0] = new FileInfo("old.txt");
+            files[1] = new FileInfo("new.txt");
+
+            Comparer comparer = new Comparer();
+
+            List<string[]> listaZmian = comparer.Compare(files);
+            foreach (string[] linia in listaZmian)
+            {
+                foreach (string pole in linia)
+                {
+                    if(String.IsNullOrEmpty(pole)) continue;
+                    Console.WriteLine(pole);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
